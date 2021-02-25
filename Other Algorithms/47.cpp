a51fff29afcbb931c113b0acbfe94548fd41ad1c
2yujeong <76840965+2yujeong@users.noreply.github.com>
2021-02-25 19:55:42 +0900

    [2차원 배열 탐색] 봉우리

---
+++ Other Algorithms/47.cpp
@@ -0,0 +1,48 @@
+/*
+높이가 자신의 상하좌우 지역의 높이보다 높은 지역을 봉우리 지역이라고 할 때 봉우리 지역의 개수를 출력하는 문제.
+2차원 배열과 2중 for문을 사용하여 각 지역마다 자신의 상하좌우 지역의 높이와 비교하였다.
+
+시간복잡도는 O(n^2)
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int map[51][51];
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int h;
+			cin >> h;
+
+			map[i][j] = h;
+		}
+	}
+	
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i > 0 && map[i - 1][j] >= map[i][j]) // 위
+				continue;
+			if (i < 50 && map[i + 1][j] >= map[i][j]) // 아래
+				continue;
+			if (j > 0 && map[i][j - 1] >= map[i][j]) // 왼쪽
+				continue;
+			if (j < 50 && map[i][j + 1] >= map[i][j]) // 오른쪽
+				continue;
+
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
